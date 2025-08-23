be6cde36b238df39c7250061b95c78ab59fe0ced
Kars Talens <karstalens@gmail.com>
2025-08-23 18:42:18 +0200

    feat: add robots.txt

--- server.js
+++ server.js
@@ -4,7 +4,6 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 const swaggerUi = require("swagger-ui-express");
 const checkApiKey = require("./apiKeyMiddleware"); // <-- add this
-
 const statsRoute = require("./routes/stats");
 require("./jobs/fetchRdw");
 require("./jobs/fetchRdwIS300H");
@@ -16,8 +15,15 @@ const app = express();
 const PORT = process.env.PORT || 5050;
 app.use(cors());
 app.use(express.json());
-
 app.use("/api/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+app.get('/robots.txt', function (req, res) {
+    res.type('text/plain');
+    res.send(
+        `User-agent: *
+Disallow: /admin`
+    );
+});
 
 app.get("/swagger.json", (req, res) => {
     res.json(swaggerDocument);
